b6f7f820e24b8892cd76460fd466a0de3a541946
ayousuf <ayousuf@salesforce.com>
2017-05-16 11:07:38 -0700

    Class to calculate interest rate

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i=2; i<9; i++) {
+            System.out.println("Interest on 10000 at the rate of " + i + "% is: " + CalculateInterest(10000, i));
+        }
+    }
+
+    public static double CalculateInterest(double Amt, double Int) {
+        return(Amt * Int/100);
+    }
+}
